32104f5e0c1c096358e885107d8bafaafc5e9300
harysreza <harysreza21@gmail.com>
2021-10-11 12:07:34 +0700

    bug category name historytopup

--- app/player/controller.js
+++ app/player/controller.js
@@ -76,7 +76,7 @@ module.exports = {
       const payload = {
         historyVoucherTopup: {
           gameName: res_voucher._doc.name,
-          category: res_voucher._doc.category ? res_voucher._doc.name : "",
+          category: res_voucher._doc.category ? res_voucher._doc.category.name : "",
           thumbnail: res_voucher._doc.thumbnail,
           coinName: res_nominal._doc.coinName,
           coinQuantity: res_nominal._doc.coinQuantity,
